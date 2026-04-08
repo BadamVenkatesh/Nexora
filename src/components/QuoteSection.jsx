3c0a30ded544d014aff536c172023aa797460258
BadamVenkatesh <venkateshbadamdurga@gmail.com>
2026-04-09 01:18:36 +0530

    fix: allow firebase env vars in netlify

--- src/components/QuoteSection.jsx
+++ src/components/QuoteSection.jsx
@@ -27,7 +27,7 @@ export default function QuoteSection() {
             <div ref={quoteRef} className="text-center max-w-xl px-4">
                 <div className="ornament mb-4 md:mb-6"><div className="ornament-icon"></div></div>
                 <blockquote className="font-serif text-lg md:text-2xl lg:text-3xl leading-snug text-cream/70 font-light italic">
-                    &ldquo;Some endings aren&rsquo;t endings&hellip; they are memories waiting to be revisited.&rdquo;
+                    &ldquo;Sometimes you will never know the value of something, until it becomes a memory.&rdquo;
                 </blockquote>
                 <p className="mt-3 md:mt-4 text-cream/25 text-[10px] tracking-[0.25em] uppercase font-sans">&mdash; Nexora</p>
                 <div className="ornament mt-4 md:mt-6"><div className="ornament-icon"></div></div>
